e38b220f893edf0edba3202c3439eb896b1ccbdd
Hariom Yadav <hoc.yadav@gmail.com>
2024-10-01 06:33:27 +0000

    Initial commit

---
+++ src/main/java/io/hari/mongodbpoc/MongodbPocApplication.java
@@ -0,0 +1,13 @@
+package io.hari.mongodbpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hari/mongodbpoc/MongodbPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.mongodbpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
